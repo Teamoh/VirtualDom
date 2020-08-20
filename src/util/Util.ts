b8cd76864738b6824cc133b4559daa63fa7fbf77
Teamoh <CR7-RM@web.de>
2020-08-20 20:44:56 +0200

    Added `isString` helper method

--- src/util/Util.ts
+++ src/util/Util.ts
@@ -2,6 +2,10 @@ export function toString(x: any) {
     return x + '';
 }
 
+export function isString(x: any) {
+    return typeof x === 'string';
+}
+
 export function isUndefined(x: any) {
     return x === undefined;
 }
