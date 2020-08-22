6edb270a6306151fd29abf9508ba2ee21656503c
Teamoh <CR7-RM@web.de>
2020-08-22 20:19:26 +0200

    Implemented method `removeChild` and `insertBefore`

--- src/classes/VNode.ts
+++ src/classes/VNode.ts
@@ -1,6 +1,7 @@
 export default class VNode {
     //#region Properties
 
+    parentNode: VNode;
     children: Array<VNode>;
 
     //#endregion
@@ -8,6 +9,7 @@ export default class VNode {
     //#region Constructor
 
     constructor() {
+        this.parentNode = null;
         this.children = [];
     }
 
@@ -15,8 +17,42 @@ export default class VNode {
 
     //#region Public Methods
 
-    appendChild(node: VNode) {
+    appendChild(node: VNode): void {
+        this.setParentNodeToThisNode(node);
         this.children.push(node);
+    }
+
+    removeChild(childNode: VNode): void {
+        const index = this.children.indexOf(childNode);
+
+        if (index === -1) {
+            return;
+        }
+
+        this.children.splice(index, 1);
+    }
+
+    insertBefore(newNode: VNode, referenceNode: VNode): void {
+        if (!referenceNode) {
+            return this.appendChild(newNode);
+        }
+
+        const referenceNodeIndex = this.children.indexOf(referenceNode);
+
+        if (referenceNodeIndex === -1) {
+            return;
+        }
+
+        this.setParentNodeToThisNode(newNode);
+        this.children.splice(referenceNodeIndex - 1, 0, newNode);
+    }
+
+    //#endregion
+
+    //#region Private Methods
+
+    private setParentNodeToThisNode(node: VNode): void {
+        node.parentNode = this;
     }
 
     //#endregion
