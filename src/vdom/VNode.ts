7200075e40756f094f8e47e81b3766bd60e0c9e8
Teamoh <CR7-RM@web.de>
2020-08-24 20:47:52 +0200

    Added getters `firstChild`, `lastChild`, `firstElementChild` and `lastElementChild`

--- src/vdom/VNode.ts
+++ src/vdom/VNode.ts
@@ -1,8 +1,43 @@
+import VElement from './VElement';
+
 export default class VNode {
-    //#region Properties
+
+    //#region Public Properties
 
     parentNode: VNode;
     children: Array<VNode>;
+
+    get firstChild(): VNode | null {
+        return this.children[0] || null;
+    }
+
+    get lastChild(): VNode | null {
+        return this.children[this.children.length - 1] || null;
+    }
+
+    get firstElementChild(): VNode | null {
+        for (let i = 0, iLen = this.children.length; i < iLen; i++) {
+            const currentChild = this.children[i];
+
+            if (currentChild instanceof VElement) {
+                return currentChild;
+            }
+        }
+
+        return null;
+    }
+
+    get lastElementChild(): VNode | null {
+        for (let i = this.children.length - 1; i > -1; i--) {
+            const currentChild = this.children[i];
+
+            if (currentChild instanceof VElement) {
+                return currentChild;
+            }
+        }
+
+        return null;
+    }
 
     //#endregion
 
