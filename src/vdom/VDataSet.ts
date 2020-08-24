86a41b26427dd41ad5f636d796b09e0fc437e244
Teamoh <CR7-RM@web.de>
2020-08-24 20:08:45 +0200

    Added `forEach` method to VDataSet

--- src/vdom/VDataSet.ts
+++ src/vdom/VDataSet.ts
@@ -1,3 +1,25 @@
-export default class VDataSet {
-    [x: string]: string;
+import ILooseObject from '../common/LooseObject';
+import { isFunction } from '../util/Util';
+
+export default class VDataSet implements ILooseObject {
+
+    //#region Public Methods
+
+    /**
+     * Executes the given callback
+     * for each attribute
+     * @param callback - The callback function to execute
+     */
+    forEach(callback: (attributeName: string, attributeValue: string) => void): void {
+        if (!isFunction(callback)) {
+            throw new TypeError('callback must be a function');
+        }
+
+        Object.keys(this).forEach((key: string) => {
+            callback.call(key, key, this[key]);
+        });
+    }
+
+    //#endregion
+
 }
